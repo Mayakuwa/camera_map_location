fea989c17da2991b8a179ad1b1423b046b103b8a
Maya <melokitty.1003@gmail.com>
2020-12-22 15:40:35 +0900

    add getPlaceAddress

--- lib/helper/location_helper.dart
+++ lib/helper/location_helper.dart
@@ -1,7 +1,17 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 const GOOGLE_APT_KEI = 'AIzaSyDx_pmIEdKVd-Gx_LDby591UshHCkzt-jw';
 
 class LocationHelper {
   static String generateLocationPreviewImage(double latitude, double longitude) {
     return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$latitude,$longitude&key=$GOOGLE_APT_KEI';
+  }
+
+  static Future<String> getPlaceAddress(double lat, double lng) async {
+    final url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=$GOOGLE_APT_KEI';
+    final response = await http.get(url);
+    return json.decode(response.body)['results'][0]['formatted_address'];
+
   }
 }
